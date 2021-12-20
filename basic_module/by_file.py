4ee14add79be8a58b0e81159fa014a7511081672
hyunjune.lim <hyunjune.lim@hyunjune-lim01.dm.corp.samsungelectronics.net>
2021-12-20 15:44:23 +0900

    악성코드 패턴 분리

---
+++ basic_module/by_file.py
@@ -0,0 +1,74 @@
+import sys
+import os
+import hashlib
+
+VirusDB =[] # virus raw data
+vdb = [] # 가공된 악성코드 : 해쉬, 악성코드명 
+vsize = [] # 악성코드 사이즈 모음 , set 자료구조 
+
+'''
+악성코드 패턴
+https://github.com/vrtadmin/clamav-devel
+
+size:hash:name
+':' 로 구분 
+'''
+
+# 파일에서 Load
+def LoadVirusDB():
+    fp = open('../data/virus_db','r')
+    while True:
+        line = fp.readline()
+        if not line : break
+        
+        line = line.strip() 
+        VirusDB.append(line)
+        print(line)
+    fp.close()
+
+# 가공하여 저장    
+def MakeVirusDB():
+    for pattern in VirusDB:
+        t = []
+        v = pattern.split(':')
+        t.append(v[1])
+        t.append(v[2])
+        vdb.append(t)
+        
+        size = int(v[0])
+        if vsize.count(size) == 0:
+            vsize.append(size)
+            
+def SearchVDB(fmd5):
+    for t in vdb:
+        if t[0] == fmd5:
+            return True, t[1]
+    return False, ''
+
+if __name__ == '__main__' :
+    LoadVirusDB()
+    MakeVirusDB()
+    
+    if len(sys.argv) != 2:
+        print("Usage : by_file [file]")
+        exit(0)
+        
+    fname = sys.argv[1]
+    size = os.path.getsize(fname)
+    if vsize.count(size) :
+        fp = open(fname, 'rb')
+        buf = fp.read()
+        fp.close()
+        
+        m = hashlib.md()
+        m.update(buf)
+        fmd5 = m.hexdigest()
+        
+        ret, vname = SearchVDB(fmd5)
+        if ret :
+            print ("%s : %s" % (fname, vname))
+            os.remove(fname)
+        else :
+            print('%s : ok' % (fname))
+    else:
+        print('%s : ok' % (fname))
