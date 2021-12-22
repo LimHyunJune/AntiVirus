aca022b2618953d6e5b14aeedbe1169449cc0f92
hyunjune.lim <hyunjune.lim@hyunjune-lim01.dm.corp.samsungelectronics.net>
2021-12-22 11:18:35 +0900

    test dummy 수정

--- basic_module/by_hash.py
+++ basic_module/by_hash.py
@@ -1,14 +1,15 @@
 import hashlib
 import os
 
-fp = open('../data/dummy_test', 'rb') # byte로 읽어야 함 
+fp = open('../data/dummy', 'rb') # byte로 읽어야 함 
 fbuf = fp.read()
 fp.close()
 
 m = hashlib.md5()
 m.update(fbuf)
 fmd5 = m.hexdigest()
-#a9030c45a2361e8565559eee400eca24
+print(fmd5)
+#30742978c615036eba5c1f4e97281dc7
 
 if fmd5 == 'a9030c45a2361e8565559eee400eca24':
     print('Dummy Test Virus')
